539eeafe5805269d11dc1d9f8afe4e19ade23ad5
Moussa Agne <moussa.agne@naturalgrass.fr>
2025-09-09 14:51:27 +0200

    ajout des fichiers de templates, static et création des modeles user , task

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstname = models.CharField(max_length = 100)
+    lastname = models.CharField(max_length = 100)
+    email  = models.EmailField(unique = True)
+    password = models.CharField(max_length = 100)
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    titre = models.CharField(max_length = 200)
+    description = models.TextField()
+    completed = models.BooleanField(default = False)
+    createad_at= models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
